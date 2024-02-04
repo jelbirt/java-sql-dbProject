11647288122d870e92e879f14a8bc0919c245832
Jake Elbirt <jelbirt@worcester.edu>
2024-02-03 19:49:15 -0500

    Initial commit

---
+++ finalProj - C++/finalProjShare.cpp
@@ -0,0 +1,6 @@
+
+
+#include <finalProjShare.hpp>
+
+
+
---
+++ finalProj - C++/finalProjShare.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdio>
+#include <cassert>
+#include <vector>
+
+
+
+#define HOST "127.0.0.1"
+#define USER "root"
+#define PASS /*put the password of MySQL in here between ""*/
